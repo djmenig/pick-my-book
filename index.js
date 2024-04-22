abc63f1cdae617e1d2aa00c78e1e20981d15e1d4
djmenig <djmenig@outlook.com>
2024-04-22 15:06:22 -0500

    change project title & re-purpose

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
