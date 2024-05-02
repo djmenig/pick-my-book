a8f4e53b24582a6c2bda84dacfc8219bb4e1e58b
djmenig <djmenig@outlook.com>
2024-05-01 22:30:51 -0500

    Adjusted color scheme, added try&catch, added comments, and updated url for a more accurate search & cover return

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// 1. set up index.ejs as a landing page (before selection)
-// 2. set up selection.ejs as the results page (after selection)
-
-
 import express from "express";
 import axios from "axios";
 
@@ -11,23 +7,34 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+//landing page
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+//selection page
 app.post("/selection", async (req, res) => {
+    try {
+    //receives a user selected subject from form and uses that for a search that returns an array of books that are borrowable, limited to 100, and is sorted randomly
     let subject = req.body.selectedSubject;
-    const randomBook = await axios.get(`https://openlibrary.org/search.json?subject=${subject}&ebook_access=borrowable&limit=100&sort=random`);
+    const randomBook = await axios.get(`https://openlibrary.org/search.json?q=subject%3A${subject}+AND+ebook_access%3Aborrowable&limit=100&sort=random`);
+    
+    //selects a random book from the above array
     const selection = randomBook.data.docs[Math.floor(Math.random() * randomBook.data.docs.length)];
-    console.log(req.body.selectedSubject);
+    console.log("Selected Subject: " + req.body.selectedSubject);
+
+    //renders the selection page with information on the selected book
     res.render("selection.ejs", {
         title: selection.title,
         author: selection.author_name,
         year: selection.first_publish_year,
         link: selection.key,
-        cover: selection.isbn[0],
+        cover: selection.cover_i,
     });
-    
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }    
 });
 
 app.listen(port, () => {
